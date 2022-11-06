8b9c4c8650a564af4196238c916fee4fa525b15d
Vyshnavi V <85625961+Vyshnavi-V@users.noreply.github.com>
2022-11-06 17:15:27 +0530

    Update script.js

--- module-4/script.js
+++ module-4/script.js
@@ -14,7 +14,7 @@ for (var i = 0; i < names.length; i++)
 {
     if(names[i].charAt(0)==='J'||names[i].charAt(0)==='j')
     {
-        console.log("Goodbye "+ names[i]);
+        console.log("Goodbye "+ names[i])
     }
     else
     {
